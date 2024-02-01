b9e541d79d4b6638f540c0bada1f494a5c5abdc7
Hugg25 <hugo.paulsson@hotmail.com>
2024-02-01 11:29:46 +0100

    hej

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+class WriteToFile{
+    public void Data()
+    {
+        StreamWriter sw = new StreamWriter("H://geeksforgeeks.txt");
+        Console.WriteLine("Enter the text that you want to write on File");
+        string str = Console.ReadLine();
+
+        sw.WriteLine(str);
+
+        sw.Flush();
+
+        sw.Close();
+    }
+    WriteToFile wr = new WriteToFile();
+}
